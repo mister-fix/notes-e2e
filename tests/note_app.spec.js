39c945945d1d7ddcacf478316ef95441dffd274f
Stephen <hellostephenwm@gmail.com>
2024-07-22 15:48:29 -0400

    refactored tests to use reset endpoint, added note importance test

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -1,7 +1,16 @@
 const { test, describe, expect, beforeEach } = require("@playwright/test");
 
 describe("Note app", () => {
-	beforeEach(async ({ page }) => {
+	beforeEach(async ({ page, request }) => {
+		await request.post("http:localhost:3001/api/testing/reset");
+		await request.post("http://localhost:3001/api/users", {
+			data: {
+				name: "Matti Luukkainen",
+				username: "mluukkai",
+				password: "salainen",
+			},
+		});
+
 		await page.goto("http://localhost:5173");
 	});
 
@@ -21,5 +30,36 @@ describe("Note app", () => {
 		await page.getByTestId("password").fill("salainen");
 		await page.getByRole("button", { name: "login" }).click();
 		await expect(page.getByText("Matti Luukkainen logged in")).toBeVisible();
+	});
+
+	describe("when logged in", () => {
+		beforeEach(async ({ page }) => {
+			await page.getByRole("button", { name: "log in" }).click();
+			await page.getByTestId("username").fill("mluukkai");
+			await page.getByTestId("password").fill("salainen");
+			await page.getByRole("button", { name: "login" }).click();
+		});
+
+		test("a new note can be created", async ({ page }) => {
+			await page.getByRole("button", { name: "new note" }).click();
+			await page.getByRole("textbox").fill("a note created by playwright");
+			await page.getByRole("button", { name: "save" }).click();
+			await expect(
+				page.getByText("a note created by playwright")
+			).toBeVisible();
+		});
+
+		describe("and a note exists", () => {
+			beforeEach(async ({ page }) => {
+				await page.getByRole("button", { name: "new note" }).click();
+				await page.getByRole("textbox").fill("another note by playwright");
+				await page.getByRole("button", { name: "save" }).click();
+			});
+
+			test("importance can be changed", async ({ page }) => {
+				await page.getByRole("button", { name: "make not important" }).click();
+				await expect(page.getByText("make important")).toBeVisible();
+			});
+		});
 	});
 });

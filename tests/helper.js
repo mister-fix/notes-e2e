0cc53cb7f31831ec852d7d235192d5ca40e4c4d1
Stephen <hellostephenwm@gmail.com>
2024-07-23 16:54:32 -0400

    added await for content to render code in the create note function

--- tests/helper.js
+++ tests/helper.js
@@ -11,4 +11,5 @@ exports.createNote = async (page, content) => {
 	await page.getByRole("button", { name: "new note" }).click();
 	await page.getByRole("textbox").fill(content);
 	await page.getByRole("button", { name: "save" }).click();
+	await page.getByText(content).waitFor();
 };
